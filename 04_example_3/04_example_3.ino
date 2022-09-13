ab031d60612acabdf97368cd36c98c15e6951725
NotInteger <qkrwjtn1236@gmail.com>
2022-09-13 15:29:26 +0900

    file add 04_example_3

---
+++ 04_example_3/04_example_3.ino
@@ -0,0 +1,28 @@
+#define PIN_LED 13
+unsigned int count, toggle;
+
+void setup() {
+  pinMode(PIN_LED,OUTPUT);
+  Serial.begin(115200);
+  while(!Serial){
+    Serial.println("시리얼 준비중");
+  }
+  Serial.println("작동 준비 완료");
+  count = toggle = 0;
+  digitalWrite(PIN_LED,toggle); // toggle = 0 즉, 불이 끈 상태.
+}
+
+void loop() {
+  Serial.println(++count);
+  toggle = toggle_state(toggle);
+  digitalWrite(PIN_LED,toggle);
+  delay(1000);
+}
+
+int toggle_state(int toggle){
+  if(toggle)
+    toggle=0;
+   else
+    toggle=1;
+   return toggle; 
+}
